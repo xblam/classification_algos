922cbeceaf79769605333980c540d60c3a413256
xblam <benlam05032004@gmail.com>
2025-06-05 11:24:52 +0700

    first commit

--- knn.py
+++ knn.py
@@ -1,3 +1,9 @@
 # first need to set up some random data points
 
 import numpy as np
+# the first thing that I need to do is to set up some random data points
+
+# need to set the dimension of the datapoints as well
+def generate_random_data(num_points=100, num_features=2):
+    X = np.random.rand(num_points, num_features) 
+    y = np.random.rand(num_points, num_features)
