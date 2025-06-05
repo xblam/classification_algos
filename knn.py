425e934307fa2a2c170c4d5c813ae3d8a65e1f91
xblam <benlam05032004@gmail.com>
2025-06-05 13:17:28 +0700

    data files imported

--- knn.py
+++ knn.py
@@ -1,9 +1,9 @@
-# first need to set up some random data points
-
 import numpy as np
-# the first thing that I need to do is to set up some random data points
 
-# need to set the dimension of the datapoints as well
-def generate_random_data(num_points=100, num_features=2):
-    X = np.random.rand(num_points, num_features) 
-    y = np.random.rand(num_points, num_features)
+"""
+* get the datapoints from kaggle
+* split the datapoints into test and training sets (maybe even validation sets?)
+* then finish training the knn
+"""
+
+# read in the csv file
